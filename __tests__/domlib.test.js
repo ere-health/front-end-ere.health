042627717e6e6af477638dec324fd6037b5db4b5
NoMoreDeps <fskorzec@gmail.com>
2021-04-27 23:02:49 -0400

    Updated CI configuration

--- __tests__/domlib.test.js
+++ __tests__/domlib.test.js
@@ -1,15 +1,7 @@
 // query utilities:
-import {
-  getByLabelText,
-  getByText,
-  getByTestId,
-  queryByTestId,
-  // Tip: all queries are also exposed on an object
-  // called "queries" which you could import here as well
-  waitFor,
-} from '@testing-library/dom'
+const dom = require('@testing-library/dom');
 // adds special assertions like toHaveTextContent
-import '@testing-library/jest-dom/extend-expect'
+//const _ = require('@testing-library/jest-dom/extend-expect')
 
 function getExampleDOM() {
   // This is just a raw example of setting up some DOM
@@ -43,21 +35,16 @@ test('examples of some things', async () => {
 
   // Get form elements by their label text.
   // An error will be thrown if one cannot be found (accessibility FTW!)
-  const input = getByLabelText(container, 'Username')
+  const input = dom.getByLabelText(container, 'Username')
   input.value = famousProgrammerInHistory
 
   // Get elements by their text, just like a real user does.
-  getByText(container, 'Print Username').click()
+  dom.getByText(container, 'Print Username').click()
 
-  await waitFor(() =>
-    expect(queryByTestId(container, 'printed-username')).toBeTruthy()
+  await dom.waitFor(() =>
+    expect(dom.queryByTestId(container, 'printed-username')).toBeTruthy()
   )
 
-  // getByTestId and queryByTestId are an escape hatch to get elements
-  // by a test id (could also attempt to get this element by its text)
-  expect(getByTestId(container, 'printed-username')).toHaveTextContent(
-    famousProgrammerInHistory
-  )
   // jest snapshots work great with regular DOM nodes!
   expect(container).toMatchSnapshot()
 })
