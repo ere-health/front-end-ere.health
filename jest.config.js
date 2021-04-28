091864a4b83519dd049cadd1318bf84a8ca963c0
François <fskorzec@outlook.com>
2021-04-27 22:54:10 -0400

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$",
-  testPathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/"],
-  moduleFileExtensions: ["js", "jsx", "mjs"]
-}
