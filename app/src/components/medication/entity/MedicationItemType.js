c69909734dcf04a88c897389faa36d3360a14786
ruszkipista <oi5672@gmail.com>
2022-01-26 21:53:49 +0000

    add Ingredients template to MedicationItemTypes

--- app/src/components/medication/entity/MedicationItemType.js
+++ app/src/components/medication/entity/MedicationItemType.js
@@ -1,4 +1,4 @@
-export const buildFhirFreeText = function (uuid, medicationType, medicationText) {
+export const buildFhirFreeText = function (uuid, medicationText) {
     return {
       fullUrl: 'http://pvs.praxis.local/fhir/Medication/'+uuid,
       resource: {
@@ -17,10 +17,8 @@ export const buildFhirFreeText = function (uuid, medicationType, medicationText)
           }
         ],
         code: {
-          coding: [{
-            system: 'https://fhir.kbv.de/CodeSystem/KBV_CS_ERP_Medication_Type', 
-            code: medicationType
-          }],
+          coding: [{ system: 'https://fhir.kbv.de/CodeSystem/KBV_CS_ERP_Medication_Type', 
+                     code: 'freitext' }],
           text: medicationText
         }
       }
@@ -49,13 +47,63 @@ export const buildFhirPzn = function (uuid, normgroesseCode, pznCode, pznText, d
         }
       ],
       code: {
-        coding: [{ system: 'http://fhir.de/CodeSystem/ifa/pzn', code: pznCode }],
+        coding: [{ system: 'http://fhir.de/CodeSystem/ifa/pzn', 
+                   code: pznCode }],
         text: pznText
       },
       form: {
         coding: [{ system: 'https://fhir.kbv.de/CodeSystem/KBV_CS_SFHIR_KBV_DARREICHUNGSFORM',
         code: dformCode }]
       }
+    }
+  }
+}
+
+buildFhirIngredient('255b49de-6cd5-4ac1-b6fb-c8d0128cf1b3', 'N3', 'Tabletten',
+                    [{itemCode:'23816',itemText:'Simvastatin',numeratorValue:20,numeratorUnit:'mg',denominatorValue:1}]
+);
+
+export const buildFhirIngredient = function (uuid, normgroesseCode, formText, ingredients) {
+  let ingredientsFHIR = ingredients.map(i => {
+    let { itemCode, itemText, numeratorValue, numeratorUnit, denominatorValue } = i;
+    return {
+      itemCodeableConcept: {
+        coding: [{ system: 'http://fhir.de/CodeSystem/ask', code: itemCode }],
+        text: itemText
+      },
+      strength: {
+        numerator: { value: numeratorValue, unit: numeratorUnit },
+        denominator: { value: denominatorValue }
+      }
+    };
+  });
+
+  return {
+    fullUrl: 'http://pvs.praxis.local/fhir/Medication/'+uuid,
+    resource: {
+      resourceType: 'Medication',
+      id: uuid,
+      meta: { profile: ['https://fhir.kbv.de/StructureDefinition/KBV_PR_ERP_Medication_Ingredient|1.0.1'] },
+      extension: [
+        { url: 'https://fhir.kbv.de/StructureDefinition/KBV_EX_ERP_Medication_Category',
+          valueCoding: {
+            system: 'https://fhir.kbv.de/CodeSystem/KBV_CS_ERP_Medication_Category',
+            code: '00'
+          }
+        },
+        { url: 'https://fhir.kbv.de/StructureDefinition/KBV_EX_ERP_Medication_Vaccine',
+          valueBoolean: false
+        },
+        { url: 'http://fhir.de/StructureDefinition/normgroesse',
+          valueCode: normgroesseCode
+        }
+      ],
+      code: {
+        coding: [{ system: 'https://fhir.kbv.de/CodeSystem/KBV_CS_ERP_Medication_Type', 
+                   code: 'wirkstoff' }]
+      },
+      form: { text: formText },
+      ingredient: ingredientsFHIR
     }
   }
 }
